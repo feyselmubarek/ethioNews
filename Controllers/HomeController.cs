ae7f77349c9b2789251c7abaed6645b9926e799b
Feysel <feyselmubarek@gmail.com>
2019-07-10 12:38:42 +0300

    Initial Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EthioNews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EthioNews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var news = new News()
+            {
+                Id = 1,
+                Category = 2,
+                DetailInformation = "bhbsjbj"
+            };
+            return View(news);
+        }
+    }
+}
---
+++ Models/News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EthioNews.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Titile { get; set; }
+        public string SourceUrl { get; set; }
+        public string SourceWebsite { get; set; }
+        public string PublishedDate { get; set; }
+        public int Category { get; set; }
+        public string ImageSource { get; set; }
+        public string DetailInformation { get; set; }
+    }
+}
